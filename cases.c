2d4674d6ea696e0cf96ef7beb1602224eed09ad9
Skalidan <53404041+Skalidan@users.noreply.github.com>
2019-07-28 18:42:12 +0300

    cases.c

---
+++ cases.c
@@ -0,0 +1,23 @@
+#include "ft.h"
+#include "stack.h"
+
+void	case_operand(char *str, t_stack *g_num, int *num_len)
+{
+	push(ft_atoi(str), g_num);
+	*num_len = digits(ft_atoi(str));
+}
+
+void	case_operator(t_stack *g_op, void (*do_op)(char c),
+	int (*precedence)(char c1, char c2), char c)
+{
+	while (!is_empty(g_op) && precedence((*g_op).stk[(*g_op).top], c))
+		do_op('.');
+	push(c, g_op);
+}
+
+void	case_closing_parenthesis(t_stack *g_op, void (*do_op)(char c))
+{
+	while ((*g_op).stk[(*g_op).top] != '(')
+		do_op('.');
+	pop(g_op);
+}
